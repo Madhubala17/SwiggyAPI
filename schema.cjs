9282cf8d510785262eba3e4eeb92608515e6cd7d
Madhubala17 <141741636+Madhubala17@users.noreply.github.com>
2024-02-13 15:53:11 +0530

    Add files via upload

---
+++ schema.cjs
@@ -0,0 +1,45 @@
+//import mongoose
+const mongoose = require('mongoose')
+//define the schema
+const restaurantsSchema = new mongoose.Schema({
+    areaName : {
+        type : String
+    },
+    avgRating : {
+        type : Number
+    },
+    costForTwo : {
+        type : String
+    },
+    cuisines : {
+        type : Array
+    },
+    name : {
+        type : String
+    }
+    })
+    // model
+const Restaurant = mongoose.model('restaurantlist', restaurantsSchema)
+
+
+    const userSchema = new mongoose.Schema({
+        contact : {
+            type : String
+        },
+        email : {
+            type : String
+        },
+        password : {
+            type : String
+        },userName : {
+            type : String
+        },
+        
+    })
+    
+    const Users = mongoose.model('userDetail', userSchema)
+    module.exports = {Restaurant,Users}
+    
+ 
+
+
