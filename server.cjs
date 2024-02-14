c9b4e6c8dd09fe313100d541cf283c7ab214f85e
Madhubala17 <madhubalas2021cs@gmail.com>
2024-02-14 14:19:00 +0530

    deleting data

--- server.cjs
+++ server.cjs
@@ -1,19 +1,19 @@
-const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const express = require('express')
 const mongoose = require('mongoose')
 
-const {Restaurant,Users} = require('./schema.cjs')
+const {Restaurant, Users} = require('./schema.cjs')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-//connection
+// Connecting with DB
 async function connectToDb() {
     try {
         await mongoose.connect('mongodb+srv://madhu:12345@madhu.441ix2h.mongodb.net/Swiggy?retryWrites=true&w=majority')
-        console.log('DB Connection Established ;)')
+        console.log('DB connection established ;)')
         const port = process.env.PORT || 8000
         app.listen(port, function() {
             console.log(`Listening on port ${port}...`)
@@ -24,57 +24,79 @@ async function connectToDb() {
     }
 }
 connectToDb()
+
 /**
- * add restaurants=post
- * get restaurants-details=get
- * create new-user=post
- * validate-user=post
- * 
+ * /add-restaurant : post
+ * /get-restaurant-details : get
+ * /update-restaurant-detail : patch
+ * /delete-restaurant-detail : delete
+ * /create-new-user : post
+ * /validate-user : post
  */
 
-app.post('/add-restaurant', async function(request,response){
-    try{
-     await Restaurant.create({
-        "areaName" : request.body.areaName,
-        "avgRating" : request.body.avgRating,
-        "costForTwo" : request.body.costForTwo,
-        "cuisines" : request.body.cuisines,
-        "name" : request.body.name
-       })
-       response.status(201).json({
+app.post('/add-restaurant', async function(request, response) {
+    try {
+        await Restaurant.create({
+            "areaName" : request.body.areaName,
+            "avgRating" : request.body.avgRating,
+            "costForTwo" : request.body.costForTwo,
+            "cuisines" : request.body.cuisines,
+            "name" : request.body.name
+        })
+        response.status(201).json({
             "status" : "success",
             "message" : "restaurant entry successful"
-       })
-    }catch(error){
+        })
+    } catch(error) {
         response.status(500).json({
             "status" : "failure",
-            "message" : "restaurant entry unsuccessful"
-
+            "message" : "restaurant entry unsuccessful",
+            "error" : error
         })
-
     }
 })
 
-app.get('/get-restaurant-details', async function(request,response){
-     try{
-         const restaurantDetails = await Restaurant.find()
-           response.status(200).json(restaurantDetails)
-     } catch(error){
+app.get('/get-restaurant-details', async function(request, response) {
+    try {
+        const restaurantDetails = await Restaurant.find()
+        response.status(200).json(restaurantDetails)
+    } catch(error) {
         response.status(500).json({
-        "status":"failure",
-        "message":"could not fetch",
-        "error":error
-
-     })
-        
-     }
+            "status" : "failure",
+            "message" : "could not fetch",
+            "error" : error
+        })
+    }
 })
 
+app.delete('/delete-restaurant-detail/:id', async function(request, response) {
+    try {
+        const restaurant = await Restaurant.findById(request.params.id)
+        if(restaurant) {
+            await Restaurant.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "deleted successfully"
+            })
+        } else { //restaurant : null
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "entry not found"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete",
+            "error" : error
+        })
+    }
+}) 
 
 app.post('/create-new-user', async function(request, response) {
     try {
          await Users.create({
-             "userName" : request.body.username,
+             "userName" : request.body.userName,
              "email" : request.body.email,
              "password" : request.body.password,
              "contact" : request.body.contact
@@ -101,7 +123,6 @@ app.post('/create-new-user', async function(request, response) {
              response.status(200).json({
                  "message" : "valid user"
              })
-            // console.log("hii.....")
          } else {
              response.status(401).json({
                  "message" : "invalid user"
